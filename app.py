5286bda2795aeb23f4627e6b66da35ecba0eac58
vinayak598 <120573174+vinayak598@users.noreply.github.com>
2024-12-24 04:49:10 +0000

    Initial commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,jsonify
 
 app = Flask(__name__)
 JOBS = [
@@ -30,6 +30,10 @@ JOBS = [
 @app.route('/')
 def hello_world():
     return render_template('home.html', jobs=JOBS, company_name='vinayak')
+
+@app.route('/api/jobs')
+def list_jobs():
+    return jsonify(JOBS)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=8080,debug=True)
